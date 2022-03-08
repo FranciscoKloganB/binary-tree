ef1a6e151b9f60959c4d96f4f65d91047c799a05
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-08 00:44:35 +0000

    test: add proper dfs test

--- tests/unit/test_dfs.py
+++ tests/unit/test_dfs.py
@@ -1,20 +1,30 @@
+from algorithms.binary_tree.dfs import traverse_depth_first_search_in_order, traverse_depth_first_search_post_order, traverse_depth_first_search_pre_order
+
+
 def test_traverse_depth_first_search_post_pre_order(
     with_one_to_seven_tree, with_tracker
 ):
-    nodes = [1, 2, 3, 4, 5, 6, 7]
+    expected = [1, 2, 3, 4, 5, 6, 7]
 
-    assert nodes == nodes
+    traverse_depth_first_search_pre_order(with_one_to_seven_tree, with_tracker)
+
+    assert with_tracker.order == expected
+
 
 
 def test_traverse_depth_first_search_post_in_order(
     with_one_to_seven_tree, with_tracker
 ):
-    nodes = [1, 2, 3, 4, 5, 6, 7]
+    expected = [3, 2, 4, 1, 6, 5, 7]
 
-    assert nodes == nodes
+    traverse_depth_first_search_in_order(with_one_to_seven_tree, with_tracker)
+
+    assert with_tracker.order == expected
 
 
 def test_traverse_depth_first_search_post_order(with_one_to_seven_tree, with_tracker):
-    nodes = [1, 2, 3, 4, 5, 6, 7]
+    expected = [3, 4, 2, 6, 7, 5, 1]
 
-    assert nodes == nodes
+    traverse_depth_first_search_post_order(with_one_to_seven_tree, with_tracker)
+
+    assert with_tracker.order == expected
