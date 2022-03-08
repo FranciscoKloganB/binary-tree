ed72fab474a9f3ee4d71b1170077f0b6cec7c406
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-08 21:50:49 +0000

    wip: test_insert_placement_order_is_correct

--- algorithms/search_tree.py
+++ algorithms/search_tree.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from domain.binary_tree_node import BinaryTreeNode
 
 
@@ -12,3 +14,13 @@ def insert(root_node: BinaryTreeNode, new_node: BinaryTreeNode):
         insert(current_node.right, new_node)
     else:
         current_node.right = new_node
+
+
+def find(root_node: BinaryTreeNode, value: Any):
+
+    if value < root_node.value and root_node.has_left:
+        find(root_node.left, value)
+    if value > root_node.value and root_node.has_right:
+        find(root_node.right, value)
+
+    return value == root_node.value
--- tests/unit/test_search_tree.py
+++ tests/unit/test_search_tree.py
@@ -1,19 +1,44 @@
+import pytest
+
 from algorithms.dfs import traverse_depth_first_search_pre_order
-from algorithms.search_tree import insert
+from algorithms.search_tree import find, insert
 from domain.binary_tree_node import BinaryTreeNode
 
-
-def test_insert(with_tracker):
-    inputs = [76, 21, 4, 32, 100, 64, 52]
-    expected = [50, 21, 4, 32, 76, 64, 52, 100]
-
+@pytest.fixture()
+def with_binary_search_tree():
     root_value = 50
     root_node = BinaryTreeNode(value=root_value)
 
-    for v in inputs:
+    remaining_values = [76, 21, 4, 32, 100, 64, 52]
+
+    for v in remaining_values:
         new_node = BinaryTreeNode(value=v)
         insert(root_node, new_node)
 
-    traverse_depth_first_search_pre_order(root_node, with_tracker)
+    return root_node
+
+def test_insert_placement_order_is_correct(with_binary_search_tree, with_tracker):
+    expected = [50, 21, 4, 32, 76, 64, 52, 100]
+
+    traverse_depth_first_search_pre_order(with_binary_search_tree, with_tracker)
 
     assert with_tracker.order == expected
+
+
+@pytest.mark.parametrize(
+    "target, expected",
+    [
+        (-1, False),
+        (0, False),
+        (1, False),
+        (2, False),
+        (3, False),
+        (4, True),
+        (21, True),
+        (52, True),
+        (100, True),
+        (1000, False),
+    ],
+)
+def test_find_is_correct(with_binary_search_tree, target, expected):
+    assert find(with_binary_search_tree, target) == expected
