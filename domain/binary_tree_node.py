33302fbf25bbf25b2e59977786711fd7778b7701
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-08 00:08:03 +0000

    fix: binary_tree_node __call__ signature

--- domain/binary_tree_node.py
+++ domain/binary_tree_node.py
@@ -34,7 +34,7 @@ class BinaryTreeCallableProgram(ABC):
     """Abstract class that enforces children to implement the __call__ method."""
 
     @abstractmethod
-    def __call__(node: BinaryTreeNode):
+    def __call__(self, node: BinaryTreeNode):
         raise NotImplementedError(
             "Classes inheriting from BinaryTreeCallableProgram must implement __call__(node: BinaryTreeNode) method"
         )
