896bed3ed93658151f4d23c904f64cdbb177fb15
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-08 21:20:32 +0000

    fix: broken test

--- tests/unit/test_binary_search_tree.py
+++ tests/unit/test_binary_search_tree.py
@@ -1,14 +1,19 @@
 from algorithms import binary_search_tree
+from algorithms.binary_tree.dfs import traverse_depth_first_search_pre_order
 from domain.binary_tree_node import BinaryTreeNode
 
 
 def test_insert(with_tracker):
     inputs = [76, 21, 4, 32, 100, 64, 52]
-    expected = [50, 76, 21, 4, 32, 100, 64, 52]
+    expected = [50, 21, 4, 32, 76, 64, 52, 100]
 
-    root_node = BinaryTreeNode(value=50)
-    for _, v in enumerate(inputs):
+    root_value = 50
+    root_node = BinaryTreeNode(value=root_value)
+
+    for v in inputs:
         new_node = BinaryTreeNode(value=v)
         binary_search_tree.insert(root_node, new_node)
 
-    assert True
+    traverse_depth_first_search_pre_order(root_node, with_tracker)
+
+    assert with_tracker.order == expected
