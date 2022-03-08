3a973be10dd0188b960f29aef4200f66683fee3b
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-08 00:16:51 +0000

    feat: add with_one_through_seven_tree fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -12,3 +12,23 @@ def with_computed_order_tracker():
             self.order.append(node.value)
 
     return ComputatedOrder()
+
+
+@pytest.fixture()
+def with_one_through_seven_tree():
+    one = BinaryTreeNode(1)
+    two = BinaryTreeNode(2)
+    three = BinaryTreeNode(3)
+    four = BinaryTreeNode(4)
+    five = BinaryTreeNode(5)
+    six = BinaryTreeNode(6)
+    seven = BinaryTreeNode(7)
+
+    one.left = two
+    one.right = five
+    two.left = three
+    two.right = four
+    five.left = six
+    five.right = seven
+    
+    return one
